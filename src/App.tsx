9a83c833406222f717868fa26418437baae42dfa
surya24-tec <surya.techietact@gmail.com>
2026-07-06 15:25:14 +0530

    Fix Github Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from "@/pages/Home";
 
 export default function App() {
   return (
-    <Router>
+    <Router  basename="/spark">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/other" element={<div className="text-center text-xl">Other Page - Coming Soon</div>} />
